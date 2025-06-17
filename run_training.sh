9679687f9e1805eff21ec3914171cbd40532d7f7
Cralence <mahlerrrr76@gmail.com>
2025-06-17 14:14:03 +0800

    update

--- run_training.sh
+++ run_training.sh
@@ -1,5 +1,6 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export WANDB_API_KEY=28b3c634497c0dc6c16767729d4719b1012a94f2
+export WANDB_MODE=disabled
 # accelerate launch --num_processes=1 train.py \
 #     --config-dir=. \
 #     --config-name=uva_libero10.yaml \
