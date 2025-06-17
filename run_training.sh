e07e6fac3cc86c4af1efe6c7a34b85b660751029
Cralence <mahlerrrr76@gmail.com>
2025-06-17 10:36:40 +0800

    update

---
+++ run_training.sh
@@ -0,0 +1,9 @@
+export CUDA_VISIBLE_DEVICES=0
+accelerate launch --num_processes=1 train.py \
+    --config-dir=. \
+    --config-name=uva_libero10.yaml \
+    model.policy.action_model_params.predict_action=False \
+    model.policy.selected_training_mode=video_model \
+    model.policy.optimizer.learning_rate=1e-4 \
+    logging.project=uva \
+    hydra.run.dir="checkpoints/uva_libero10_video_model"
