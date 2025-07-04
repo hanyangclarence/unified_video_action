fe39709a450e2f3f987bc07126d117c1aefa527f
Cralence <mahlerrrr76@gmail.com>
2025-07-04 23:42:25 +0800

    update

--- run_slurm_training.sh
+++ run_slurm_training.sh
@@ -18,10 +18,18 @@ export WANDB_API_KEY=28b3c634497c0dc6c16767729d4719b1012a94f2
 
 export MUJOCO_PY_MUJOCO_PATH=/gpfs/u/home/LMCG/LMCGhazh/scratch/yanghan/embodied_o1/mujoco/mujoco210/bin
 
+# LD_LIBRARY_PATH="/usr/lib/nvidia:$MUJOCO_PY_MUJOCO_PATH:$CONDA_PREFIX/lib:$LD_LIBRARY_PATH" torchrun --nnode=1 --nproc_per_node=8 train.py --config-dir=. --config-name=uva_rlbench.yaml \
+#     model.policy.action_model_params.predict_action=False \
+#     model.policy.selected_training_mode=video_model \
+#     model.policy.optimizer.learning_rate=1e-4 \
+#     logging.project=uva \
+#     hydra.run.dir="checkpoints/uva_rlbench_video_model" \
+#     training.resume=True
+
 LD_LIBRARY_PATH="/usr/lib/nvidia:$MUJOCO_PY_MUJOCO_PATH:$CONDA_PREFIX/lib:$LD_LIBRARY_PATH" torchrun --nnode=1 --nproc_per_node=8 train.py --config-dir=. --config-name=uva_rlbench.yaml \
-    model.policy.action_model_params.predict_action=False \
-    model.policy.selected_training_mode=video_model \
+    model.policy.action_model_params.predict_action=True \
+    model.policy.autoregressive_model_params.pretrained_model_path="checkpoints/uva_rlbench_video_model/checkpoints/best.ckpt" \
     model.policy.optimizer.learning_rate=1e-4 \
     logging.project=uva \
-    hydra.run.dir="checkpoints/uva_rlbench_video_model" \
-    training.resume=True
+    hydra.run.dir="checkpoints/uva_rlbench_video_act_model" \
+    checkpoint.topk.monitor_key="val_action_l2_distances"
