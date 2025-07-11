1f664afa020ddf843c2597994aedbab67f46007b
Cralence <mahlerrrr76@gmail.com>
2025-07-11 09:29:51 +0800

    update

--- run_bv.sh
+++ run_bv.sh
@@ -2,23 +2,23 @@ export WANDB_API_KEY=28b3c634497c0dc6c16767729d4719b1012a94f2
 export DEBUG=0
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
-accelerate launch --num_processes=8 train.py \
-        --config-dir=. \
-        --config-name=uva_rlbench_pn.yaml \
-        model.policy.action_model_params.predict_action=False \
-        model.policy.autoregressive_model_params.pretrained_model_path=pretrained_models/libero10.ckpt \
-        model.policy.selected_training_mode=video_model \
-        model.policy.optimizer.learning_rate=1e-4 \
-        logging.project=uva_pn \
-        hydra.run.dir="checkpoints/uva_rlbench_pn_video_model_new"
-
-
 # accelerate launch --num_processes=8 train.py \
 #         --config-dir=. \
 #         --config-name=uva_rlbench_pn.yaml \
-#         model.policy.action_model_params.predict_action=True \
-#         model.policy.autoregressive_model_params.pretrained_model_path="checkpoints/uva_rlbench_pn_video_model_new/checkpoints/latest.ckpt" \
+#         model.policy.action_model_params.predict_action=False \
+#         model.policy.autoregressive_model_params.pretrained_model_path=pretrained_models/libero10.ckpt \
+#         model.policy.selected_training_mode=video_model \
 #         model.policy.optimizer.learning_rate=1e-4 \
 #         logging.project=uva_pn \
-#         hydra.run.dir="checkpoints/uva_rlbench_pn_video_act_model_new" \
-#         checkpoint.topk.monitor_key="val_action_l2_distances"
+#         hydra.run.dir="checkpoints/uva_rlbench_pn_video_model_new"
+
+
+accelerate launch --num_processes=8 train.py \
+        --config-dir=. \
+        --config-name=uva_rlbench_pn.yaml \
+        model.policy.action_model_params.predict_action=True \
+        model.policy.autoregressive_model_params.pretrained_model_path="checkpoints/uva_rlbench_pn_video_model_new/checkpoints/latest.ckpt" \
+        model.policy.optimizer.learning_rate=1e-4 \
+        logging.project=uva_pn \
+        hydra.run.dir="checkpoints/uva_rlbench_pn_video_act_model_new" \
+        checkpoint.topk.monitor_key="val_action_l2_distances"
